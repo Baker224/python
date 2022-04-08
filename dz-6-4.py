438c5509f889f647dff38ae909a4b17dd63fb5d2
Timur Minikhairov / Тимур Минихаиров <32126793+Baker224@users.noreply.github.com>
2022-04-08 20:41:29 +0500

    Add files via upload

---
+++ dz-6-4.py
@@ -0,0 +1,14 @@
+from itertools import zip_longest
+
+with open('users_hobby.txt', 'w', encoding='utf-8') as f:
+    file_users = open('users.csv', encoding='utf-8')
+    file_hobby = open('hobby.csv', encoding='utf-8')
+    num_lines_users = sum(1 for line in file_users)
+    num_lines_hobby = sum(1 for line in file_hobby)
+    if num_lines_users < num_lines_hobby:
+        exit(1)
+    file_users.seek(0)
+    file_hobby.seek(0)
+    for line_user, line_user_hobby in zip_longest(file_users, file_hobby):
+        f.write(f'{line_user.strip()}: '
+            f'{line_user_hobby.strip() if line_user_hobby is not None else line_user_hobby}\n')
